a3c8ce45584d2db0fcf6295a1a57752803be3ee6
pvlopes <90511486+loppesz@users.noreply.github.com>
2024-03-20 17:07:04 -0300

    Update atividade1.js

--- atividade1.js
+++ atividade1.js
@@ -1,35 +1,27 @@
-//calculadora
 const prompt = require('prompt-sync')();
 
 let op
 let n1;
 
+console.log('Olá!')
+console.log('Selecione o código de sua região!!')
 
-console.log('Olá')
-console.log('Selecione o código de sua região')
-
-op = parseInt(prompt ('Digite o Código '));
+op = parseInt(prompt ('Digite o Código:  '));
 
 switch(op){
     case 1: 
-        soma=n1*1;
         console.log('Sul' );
-        
         break;
     case 2:
-        soma=n1*2;
         console.log('Norte ');
         break;
     case 3:
-        soma=n1*3;
         console.log('leste ');
         break;
     case 4:
-        soma=n1*4;
         console.log('Oeste');
         break;
     case 5:
-        soma=n1*5;
         console.log('Nordeste' );
         break;
     case 6:
